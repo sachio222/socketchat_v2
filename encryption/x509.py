fc7299b84eb9a4b5950c62419c70a50897818d65
sachio222 <brainpowerux@gmail.com>
2020-08-15 23:22:40 -0400

    updates to x509

--- encryption/x509.py
+++ encryption/x509.py
@@ -20,6 +20,9 @@ from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import rsa
 
+from cryptography.x509.oid import NameOID
+from cryptography.hazmat.primitives import hashes
+
 rsa_key_path = 'encryption/keys/TLS/rsa_key.pem'
 cert_path = 'encryption/keys/TLS/certificate.pem'
 
@@ -49,41 +52,40 @@ def load_private_rsa_key(path=rsa_key_path):
     # serialization.load_pem_private_key()
     pass
 
+def self_sign_cert(key):
+    # Self signing.
+    # Provide signing details.
+    subject = issuer = x509.Name([
+        # Provide various details about who we are for request from CA.
+        x509.NameAttribute(NameOID.COUNTRY_NAME, u'US'),
+        x509.NameAttribute(NameOID.STATE_OR_PROVINCE_NAME, u'California'),
+        x509.NameAttribute(NameOID.LOCALITY_NAME, u'San Mateo'),
+        x509.NameAttribute(NameOID.ORGANIZATION_NAME, u'Company Inc.'),
+        x509.NameAttribute(NameOID.COMMON_NAME, u'ubuntu')
+    ])
+
+    cert = x509.CertificateBuilder().subject_name(
+        subject
+    ).issuer_name(
+        issuer
+    ).public_key(
+        # Send request with public key.
+        key.public_key()
+    ).serial_number(
+        x509.random_serial_number()
+    ).not_valid_before(
+        datetime.datetime.utcnow()   
+    ).not_valid_after(
+        datetime.datetime.utcnow() + datetime.timedelta(days=10)
+    ).add_extension(
+        x509.SubjectAlternativeName([x509.DNSName(u'ubuntu')]),
+        critical=False
+    # Sign certificate with private key
+    ).sign(key, hashes.SHA256(), default_backend())
+    # Write certificate to disk
+    with open(cert_path, 'wb') as f:
+        f.write(cert.public_bytes(serialization.Encoding.PEM))
+
+
 key = generate_private_rsa_key()
-
-from cryptography.x509.oid import NameOID
-from cryptography.hazmat.primitives import hashes
-
-# Self signing.
-
-# Provide signing details.
-subject = issuer = x509.Name([
-    # Provide various details about who we are for request from CA.
-    x509.NameAttribute(NameOID.COUNTRY_NAME, u'US'),
-    x509.NameAttribute(NameOID.STATE_OR_PROVINCE_NAME, u'California'),
-    x509.NameAttribute(NameOID.LOCALITY_NAME, u'San Mateo'),
-    x509.NameAttribute(NameOID.ORGANIZATION_NAME, u'Company Inc.'),
-    x509.NameAttribute(NameOID.COMMON_NAME, u'ubuntu')
-])
-
-cert = x509.CertificateBuilder().subject_name(
-    subject
-).issuer_name(
-    issuer
-).public_key(
-    # Send request with public key.
-    key.public_key()
-).serial_number(
-    x509.random_serial_number()
-).not_valid_before(
-    datetime.datetime.utcnow()   
-).not_valid_after(
-    datetime.datetime.utcnow() + datetime.timedelta(days=10)
-).add_extension(
-    x509.SubjectAlternativeName([x509.DNSName(u'ubuntu')]),
-    critical=False
-# Sign certificate with private key
-).sign(key, hashes.SHA256(), default_backend())
-# Write certificate to disk
-with open(cert_path, 'wb') as f:
-    f.write(cert.public_bytes(serialization.Encoding.PEM))
+self_sign_cert(key)
