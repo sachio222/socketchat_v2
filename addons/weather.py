4a23c7b9ee8f217beae2b6a0c16cbcf92078bcd9
sachio222 <brainpowerux@gmail.com>
2020-08-20 14:06:53 -0400

    full weather report added

--- addons/weather.py
+++ addons/weather.py
@@ -5,6 +5,7 @@ def report(msg):
 
     loc = ''
     weather = "-!- Can't reach weather service right meow."
+    print_lines = None
 
     try:
         # loc = input('-?- Where? (press enter for ip location): ')
@@ -12,8 +13,14 @@ def report(msg):
         if '1line' in msg:
             msg.remove('1line')
             fmt = 'format=4'
+        elif 'full' in msg:
+            msg.remove('full')
+            fmt = ''
+            print_lines = -1
+
         else:
             fmt = ''
+            print_lines = 7
 
         if msg[1:]:
             loc = ' '.join(msg[1:])
@@ -22,8 +29,9 @@ def report(msg):
         weather = requests.get(url)
 
         print('\r-=- ', end='')
-        for line in weather.text.splitlines()[:7]:
+        for line in weather.text.splitlines()[:print_lines]:
             print(line)
+
 
     except:
         pass
