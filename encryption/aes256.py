4ceb0ef849ad39be0aacc30701734d12ced41dd6
sachio222 <brainpowerux@gmail.com>
2020-08-20 01:13:47 -0400

    fixed nonce issue

--- encryption/aes256.py
+++ encryption/aes256.py
@@ -9,9 +9,10 @@ key_path = 'encryption/keys/aes256/secret.key'
 class AES256Cipher():
     def __init__(self):
         self.backend = default_backend()
-        self.iv = secrets.token_bytes(16)
+        self.iv = self.new_iv()
         self.key = self.generate_key()
         self.cipher = self.new_cipher(self.key, self.iv, self.backend)
+        self.nonce = None
 
     def generate_key(self):
         key = secrets.token_bytes(32)
@@ -25,28 +26,33 @@ class AES256Cipher():
         cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=backend)
         return cipher
 
-    def new_iv(self, iv) -> int:
-        # iv = int(iv)
-        # iv += 1
-        # iv = hex(iv)
+    def new_iv(self) -> int:
+        iv = secrets.token_bytes(16)
         return iv
 
-    def padder(self, msg: bytes, size: int=128):
+    def padder(self, msg: bytes, size: int=128) -> bytes:
         padder = padding.ANSIX923(size).padder()
         padded_data = padder.update(msg)
         padded_data += padder.finalize()
         return padded_data
 
+    def unpadder(self, padded_data: bytes, size: int=128) -> bytes:
+        unpadder = padding.ANSIX923(128).unpadder()
+        data = unpadder.update(padded_data)
+        data = data + unpadder.finalize()
+        return data 
+
     def encrypt(self, msg: bytes) -> bytes:
-        cipher = self.new_cipher(self.key, self.new_iv(self.iv), self.backend)
-        self.iv = self.new_iv(self.iv)
+        self.nonce = self.new_iv()
+        print('The nonce:', self.nonce)
+        cipher = self.new_cipher(self.key, self.nonce, self.backend)
         print('iv:', self.iv)
         encryptor = cipher.encryptor()
         cipher_txt = encryptor.update(msg) + encryptor.finalize()
         return cipher_txt
 
     def decrypt(self, cipher_txt: bytes) -> bytes:
-        cipher = self.new_cipher(self.key, self.new_iv(self.iv), self.backend)
+        cipher = self.new_cipher(self.key, self.nonce, self.backend)
         decryptor = cipher.decryptor()
         msg = decryptor.update(cipher_txt) + decryptor.finalize()
         return msg
@@ -58,32 +64,40 @@ class AES256Cipher():
 
 if __name__ == "__main__":
     aes = AES256Cipher()
-    print(aes.key)
-    print(aes.iv)
-    print(aes.cipher)
-    msg = b'sup boo pp '
+    # print(aes.key)
+    # print(aes.iv)
+    # print(aes.cipher)
+    msg = b"What is the question at hand? Do we evven really know?? What do we do if the message gets even huger? That's a big question to think about isn't it? Hmm but that's actually pretty cool, it is decrypting."
     msg = aes.padder(msg, 128)
-    enc_msg = aes.encrypt(msg)
-    print(enc_msg)
+    msg = aes.encrypt(msg)
+    # print(enc_msg)
+    msg = aes.decrypt(msg)
+    msg= aes.unpadder(msg)
+    print(msg)
+    # msg = aes.padder(msg, 128)
+    # enc_msg = aes.encrypt(msg)
+    # # print(enc_msg)
+    # pt = aes.decrypt(enc_msg)
+    # pt = aes.unpadder(pt)
 
-    pt = aes.decrypt(enc_msg)
-    print(pt)
-    msg = aes.padder(msg, 128)
-    enc_msg = aes.encrypt(msg)
-    print(enc_msg)
-    pt = aes.decrypt(enc_msg)
-    msg = aes.padder(msg, 128)
-    enc_msg = aes.encrypt(msg)
-    print(enc_msg)
-    pt = aes.decrypt(enc_msg)
-    msg = aes.padder(msg, 128)
-    enc_msg = aes.encrypt(msg)
-    print(enc_msg)
-    pt = aes.decrypt(enc_msg)
-    msg = aes.padder(msg, 128)
-    enc_msg = aes.encrypt(msg)
-    print(enc_msg)
-    pt = aes.decrypt(enc_msg)
+    # msg = aes.padder(msg, 128)
+    # enc_msg = aes.encrypt(msg)
+    # # print(enc_msg)
+    # pt = aes.decrypt(enc_msg)
+    # pt = aes.unpadder(pt)
+    # print(pt)
+    # msg = aes.padder(msg, 128)
+    # enc_msg = aes.encrypt(msg)
+    # # print(enc_msg)
+    # pt = aes.decrypt(enc_msg)
+    # pt = aes.unpadder(pt)
+    # print(pt)
+    # msg = aes.padder(msg, 128)
+    # enc_msg = aes.encrypt(msg)
+    # # print(enc_msg)
+    # pt = aes.decrypt(enc_msg)
+    # pt = aes.unpadder(pt)
+    # print(pt)
 
     
 
