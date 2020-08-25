e460f52ea7c4697b38e6890456866d0cbc952933
sachio222 <brainpowerux@gmail.com>
2020-08-25 12:30:50 -0400

    cnxn msg

--- client.py
+++ client.py
@@ -371,7 +371,7 @@ class Client(ChatIO):
         aes_key = aes.hex_to_b64(aes_key)
         enc_key = nacl.put_in_secret_box(self.secret_box, aes_key)
         print('encoded aes_key:', enc_key)
-        enc_key = nacl.encode_b64(enc_key)
+        enc_key = Base64Encoder.encode(enc_key)
         self.pack_n_send(sock, 'x', enc_key)
         del aes_key
 
