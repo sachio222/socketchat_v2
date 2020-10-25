fddd79553c85968e1606ae125a2be30e9e28ef6c
sachio222 <brainpowerux@gmail.com>
2020-10-24 23:08:41 -0400

    changes

--- lib/encryption/CipherTools.py
+++ lib/encryption/CipherTools.py
@@ -68,10 +68,10 @@ def unpack_keys_from_xfer(key_pack_hex:hex, path=paths.nacl_keys,
         print("key_dict is", key_dict)
 
         aes_key = key_dict["aes"]
-        AES256Cipher().write_key(aes_key)
+        AES256Cipher().write_key(aes_key.encode())
 
-        # fernet_key = key_dict["fernet"]
-        # FernetCipher().write_key(fernet_key)
+        fernet_key = key_dict["fernet"]
+        FernetCipher().write_key(fernet_key.encode())
 
         # chacha_key = key_dict["chacha"]
         # XChaCha20Poly1305.write_key(chacha_key)
